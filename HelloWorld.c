796a6a9ec661d947f44dce635d43155e208b7875
ismailalpha1 <ijalali626@gmail.com>
2024-10-16 11:43:19 +0200

    Variables

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
-
-    printf("Hello world \n");
+    //variables
+    int d = 5; //%d for int 
+    char c = 'C'; // %c for single character
+    char name = "Ismail";//%s for array of characters
+    float gpa = 2.013 ;//%f for float
+    printf("Hello world %d and %c \n", d,c);
+    printf("My name is %s",name);
     return 0;
 }
