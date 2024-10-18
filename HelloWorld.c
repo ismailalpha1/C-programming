50ccb3f0c9e138e47b450b76bb30d0b9758d2dd0
ismailalpha1 <ijalali626@gmail.com>
2024-10-18 19:17:58 +0200

    Scanf

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,11 +2,26 @@
 
 int main(){
     //variables
-    int d = 5; //%d for int 
-    char c = 'C'; // %c for single character
-    char name = "Ismail";//%s for array of characters
-    float gpa = 2.013 ;//%f for float
-    printf("Hello world %d and %c \n", d,c);
-    printf("My name is %s",name);
+    // int d = 5; //%d for int 
+    // char c = 'C'; // %c for single character
+    // char name[] = "Ismail";//%s for array of characters
+    // float gpa = 2.013 ;//%f for float
+    // printf("Hello world %d and %c \n", d,c);
+    // printf("My name is %s",name);
+   
+
+    //scanf
+    // int age;
+    // char name[25];
+    // printf("Who are you?");
+    // fgets(name,25,stdin); //for arrays
+    // name[strlen(name)-1] = '\0';
+    // printf("How old are you?");
+    // scanf("%d",&age);
+
+    // printf("\n Hello %s", name);
+    // printf("\n You are %d old", age);
+
+
     return 0;
 }
